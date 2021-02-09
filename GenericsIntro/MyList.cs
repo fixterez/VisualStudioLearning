a601865f0b212e094b2eb269bae560db1a4a4ff5
Orçun Koca <orcunkoca1@outlook.com>
2021-02-09 15:25:22 +0300

    09.02.2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //Constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+       public void Add(T item)
+        {
+            // Geçici Array
+            T[] tempArray = items; //
+            items = new T[items.Length+1];
+            
+            for (int i = 0; i < tempArray.Length; i++) // tempArray'de tutulan verileri geri almak.
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item; // eklenmek istenilen elemanı şu an ekleyebilirim.
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> alphabets = new MyList<string>();
+            alphabets.Add("A");
+        }
+    }
+}
