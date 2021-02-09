28b543344d5bb92d5eecc0f0a348924bd37f0c25
Orçun Koca <orcunkoca1@outlook.com>
2021-02-09 20:28:53 +0300

    version

---
+++ GenericsYapisi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("İstanbul");
+        }
+    }
+    class MyList<T> // Generic Class
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+
+        }
+    }
+}
