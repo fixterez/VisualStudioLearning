9eba7fa97fe15b83c63f8a7c88f8b2e107538139
Orçun Koca <orcunkoca1@outlook.com>
2021-02-09 14:09:35 +0300

    Add project files.

---
+++ VisualStudioLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FirstProject");
+        }
+    }
+}
