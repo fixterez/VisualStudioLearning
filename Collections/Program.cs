32097b260f3f98654adc7450701557210a491490
Orçun Koca <orcunkoca1@outlook.com>
2021-02-09 14:40:17 +0300

    09.02.2021

---
+++ Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Collections
+            List<String> alphabets = new List<string> { "a", "b", "c", "d" };
+            Console.WriteLine(alphabets[0]);
+            Console.WriteLine(alphabets[1]);
+            Console.WriteLine(alphabets[2]);
+            Console.WriteLine(alphabets[3]);
+            alphabets.Add("e");
+            Console.WriteLine(alphabets[4]);
+            Console.WriteLine(alphabets[4]);
+
+        }
+    }
+}
